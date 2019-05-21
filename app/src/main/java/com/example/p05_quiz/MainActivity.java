fb8e445675a1c17a90299b372be28200fdaa737e
17044513 <17044513@myrp.edu.sg>
2019-05-21 15:44:17 +0800

    Testing again

--- app/src/main/java/com/example/p05_quiz/MainActivity.java
+++ app/src/main/java/com/example/p05_quiz/MainActivity.java
@@ -2,15 +2,34 @@ package com.example.p05_quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button decrementButton;
+    Button incrementButton;
+    Button submitButton;
+    CheckBox checkBox1;
+    CheckBox checkBox2;
+    EditText quantity_textView;
+
+    int pax=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    //
-        //
+        decrementButton=findViewById(R.id.decrementButton);
+        incrementButton=findViewById(R.id.incrementButton);
+        submitButton=findViewById(R.id.submitButton);
+        quantity_textView=findViewById(R.id.quantity_textview);
+        checkBox1=findViewById(R.id.checkBox1);
+        checkBox2=findViewById(R.id.checkBox2);
+
+        quantity_textView.setText(pax);
+
     }
 }
